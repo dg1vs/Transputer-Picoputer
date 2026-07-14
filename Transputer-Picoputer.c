a28d8e9b77013b25b07dd042d13e49444e234ab7
Dr. Karsten Schmidt <15368701+dg1vs@users.noreply.github.com>
2026-07-14 21:29:42 +0200

    init

---
+++ Transputer-Picoputer.c
@@ -0,0 +1,480 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/gpio.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
+#include "processor.h"
+#include "arithmetic.h"
+#include "server.h"
+#include "transputer_programs.h"
+#include "pico_transputer_config.h"
+
+#include "oled096.h"
+
+#include "picoputer.pio.h"
+
+#if TRANS_USE_SPI_RAM
+   #include "spi_ram.h"
+#endif
+
+
+#define DEBUG_STOP          \
+    do                      \
+    {                       \
+        volatile int x = 1; \
+        while (x)           \
+        {                   \
+        }                   \
+    } while (0)
+
+#define MEM_BYTE_MASK 0x0000ffff
+
+static I2C_SLAVE_DESC oled0 = {
+    .i2c = TP3_OLED_I2C,
+    .sda_pin = TP3_OLED_SDA_PIN,
+    .scl_pin = TP3_OLED_SCL_PIN,
+    .slave_7bit_addr = TP3_OLED_ADDRESS,
+    .baudrate = TP3_OLED_BAUDRATE,
+};
+
+extern unsigned long WPtr;
+extern unsigned long ProcPriority;
+
+extern void server_simkey(void);
+
+void link_in_booting_ack(int i);
+void link_in_booting_data(int i, int data);
+
+#if 0
+/* Write a byte to memory. */
+inline void writebyte (unsigned long ptr, unsigned char value)
+{
+	/* Write byte, ensuring memory reference is in range. */
+	mem[(ptr & MEM_BYTE_MASK)]   = value;
+}
+#endif
+
+int analyse = false;
+int copy = false;
+int exitonerror = false;
+int peeksize = 8;
+
+int membits = 0;
+
+uint32_t profile[10];
+int profiling = false;
+int tracing = 0;
+int emudebug = false;
+int memdebug = false;
+int memnotinit = false;
+int msgdebug = false;
+char *dbgtrigger = NULL;
+int usetvs = false;
+
+
+void init_transputer(void)
+{
+    Txxx = 800;
+
+    CoreSize = TRANS_CORE_SIZE;
+    MemStart = TRANS_MEM_START;
+    ExtMemStart = TRANS_EXT_MEM_START;
+
+#if TRANS_USE_SPI_RAM
+    spi_ram_init_default();
+#endif
+
+    printf("Transputer memory backend: %s\n", TRANS_MEM_BACKEND_NAME);
+    printf("Core RAM: %lu bytes, external emulated RAM: %lu bytes\n",
+           (unsigned long)TRANS_CORE_SIZE,
+           (unsigned long)TRANS_EXT_MEM_SIZE);
+
+    while ((WPtr & 0x00000003) != 0x00000000)
+        WPtr++;
+
+    ProcPriority = LoPriority;
+
+#if 0
+  /* Initialise profiling array. */
+  for (temp=0; temp<10; temp++)
+    {
+    profile[temp] = 0;
+    }
+#endif
+
+    // Start the transputer going
+    // mainloop();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Boot sequence handlers
+//
+////////////////////////////////////////////////////////////////////////////////
+
+int boot_write_index = 0;
+int boot_length_remaining = 0;
+int boot_link = -1;
+int booting = 0;
+int boot_done = 0;
+
+void link_in_boot_load(int i, int data)
+{
+    // Another byte has been received, load into memory
+    //printf("\nWriting %02X to %08X", data, MemStart + boot_write_index);
+
+    writebyte_int((MemStart + boot_write_index), data);
+
+    // Update
+    WPtr = MemStart + boot_write_index;
+
+    // Point to next byte
+    boot_write_index++;
+
+    // One more byte done
+    boot_length_remaining--;
+
+    if (boot_length_remaining == 0)
+    {
+        // we are done
+        boot_done = 1;
+    }
+}
+
+// First data byte received, this is the length
+
+void link_in_boot_start_data(int i, int data)
+{
+
+    // We have a length byte on a link, start loading data into memory
+    boot_link = i;
+
+    printf("Boot start data (length):%02X\n", data);
+
+    if (data >= 2)
+    {
+        boot_length_remaining = data;
+
+        // ACK the byte
+        send_ack_to_link(i);
+
+        /* One compact diagnostic line per bootstrap. */
+        printf("B:%02X\n", data);
+
+        // Switch to the memory loader handler from now on
+        booting = 1;
+
+        printf("Booting, moving to download handlers...\n");
+        link_in_data_fp = link_in_booting_data;
+        link_in_ack_fp = link_in_booting_ack;
+    }
+    else
+    {
+        // This is a debug peek or poke
+        // Handle later maybe
+    }
+}
+
+void link_in_boot_start_ack(int i)
+{
+    // This shouldn't happen, just ignore it
+}
+
+//--------------------------------------------------------------------------------
+// We are now loading bytes into memory
+
+void link_in_booting_data(int i, int data)
+{
+    // We only deal with the link that we ar ebooting from
+    if (i == boot_link)
+    {
+        // Store byte in memory
+        link_in_boot_load(i, data);
+
+        // ACK the byte
+        send_ack_to_link(i);
+    }
+    else
+    {
+        // Ignore
+    }
+}
+
+void link_in_booting_ack(int i)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Startup the Picoputer
+//
+////////////////////////////////////////////////////////////////////////////////
+
+unsigned char byte_int(uint32_t ptr);
+INLINE void writebyte_int(uint32_t ptr, unsigned char value);
+
+int main()
+{
+    stdio_init_all();
+
+    char line[24];
+
+    uint linkout_sm;
+    PIO linkout_pio;
+
+    set_sys_clock_khz(TP3_SYS_CLOCK_KHZ, true);
+
+    /* INMOS links are inactive at logic low. Hold LinkOut low immediately. */
+    gpio_init(TP3_LINK_OUT_PIN);
+    gpio_put(TP3_LINK_OUT_PIN, 0);
+    gpio_set_dir(TP3_LINK_OUT_PIN, GPIO_OUT);
+
+    bi_decl(bi_program_description("This is a test binary."));
+    bi_decl(bi_1pin_with_name(TP3_LED_PIN, "On-board LED"));
+
+
+    printf("\nPicoputer\n");
+
+    oled_setup(&oled0);
+
+    oled_set_xy(&oled0, 0, 0);
+    oled_display_string(&oled0, "Picoputer");
+
+    oled_set_xy(&oled0, 0, 14);
+    oled_display_string(&oled0, "I2C OLED Display");
+
+    uint32_t sys_clock_hz = clock_get_hz(clk_sys);
+    snprintf(line, sizeof(line), "%lu Hz", (unsigned long)sys_clock_hz);
+    oled_set_xy(&oled0, 0, 21);
+    oled_display_string(&oled0, line);
+
+    
+    printf("clk_sys: %lu Hz\n", (unsigned long)clock_get_hz(clk_sys));
+
+    printf("\nPicoputer GPIO init\n");
+
+    gpio_init(TP3_LED_PIN);
+    gpio_set_dir(TP3_LED_PIN, GPIO_OUT);
+    gpio_put(TP3_LED_PIN, 0);
+
+    /*
+     * The generated PIO init helpers configure GPIO function and direction.
+     * Do not assign GPIO_FUNC_PIO0/PIO1 manually here: TP3_LINK_PIO is the
+     * single source of truth for the selected PIO instance.
+     */
+
+#if 1
+
+    // Initialise the link server
+    server_init();
+
+    // PIO stuff
+    // todo get free sm
+    PIO pio = TP3_LINK_PIO;
+
+    // exit(0);
+    //  Choose which PIO instance to use (there are two instances)
+
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offset = pio_add_program(pio, &picoputerclk_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint sm = pio_claim_unused_sm(pio, true);
+    picoputerclk_program_init(pio, sm, offset, TP3_LINK_CLOCK_PIN);
+
+    //--------------------------------------------------------------------------------
+    // Set up link out state machine
+
+    sm = pio_claim_unused_sm(pio, true);
+
+    linkout_sm = sm;
+    linkout_pio = pio;
+
+    offset = pio_add_program(pio, &picoputerlinkout_program);
+    picoputerlinkout_program_init(pio, sm, offset, TP3_LINK_OUT_PIN);
+
+    // Tie state machine to link
+    server_linkout_init(0, pio, sm);
+
+    // The link out should now be running. Send a byte to the link. The IMSC011
+    // should see that byte, and present it on its Q outputs. It should then take QVALID
+    // active, which the Mega monitor should see.
+
+    sm = pio_claim_unused_sm(pio, true);
+    uint linkin_sm = sm;
+    PIO linkin_pio = pio;
+    offset = pio_add_program(pio, &picoputerlinkin_program);
+    picoputerlinkin_program_init(pio, sm, offset, TP3_LINK_IN_PIN);
+
+    // Tie state machine to link
+    server_linkin_init(0, pio, sm);
+
+    //--------------------------------------------------------------------------------
+    //
+    // Set up link in state machine
+    //
+
+    int qval = 0;
+    uint32_t data;
+
+    //--------------------------------------------------------------------------------
+    // Initialise transputer emulator
+    //
+
+    init_transputer();
+
+    // Set up a keypress to kick the in intruction
+    server_simkey();
+
+#define LOOPBACK 0
+
+    // Wait for things to settle (link adapter clock etc)
+    sleep_ms(500);
+
+    // We sit in a bootstrap mode, waiting for data over the link
+    // That data is a standard transputer 'boot from link' stream.
+    //
+    // Wait for a byte on a link (just link 0 for now)
+    // If byte >= 2 then load byte number of bytes into memory from
+    // the link
+    // If <2 then debug features are handled
+
+    // Wait for a byte from the link
+    // Set up data and ack handlers
+
+    link_in_data_fp = link_in_boot_start_data;
+    link_in_ack_fp = link_in_boot_start_ack;
+
+    printf("Entering boot loop...\n");
+
+    while (!boot_done)
+    {
+        // Process links
+        bootloop();
+    }
+
+    printf("Entering main loop...\n");
+
+    while (1)
+    {
+        char line[80];
+
+        //----------------------------------------
+        // Run the transputer
+#if !LOOPBACK
+        mainloop();
+
+        // And the links, the server is now on whatever is on the other
+        // end of the links
+
+        linkloop();
+#endif
+        //----------------------------------------
+#if LOOPBACK
+        qval++;
+
+        if (data = picoputerlinkin_get(linkin_pio, linkin_sm))
+        {
+            // We have data. The external LinkIn signal is not inverted.
+            data >>= 22;
+
+            sprintf(line, "\ndata= %08X", data);
+            printf(line);
+
+            if (data == 0)
+            {
+                // This is an ACK
+                printf("\nACK");
+            }
+            else
+            {
+                // Data packet
+                // Remove second stop bit in LSB
+                data >>= 1;
+
+                // Mask out data, just in case
+                data &= 0xff;
+
+                printf("\nDATA:%02X", data);
+            }
+
+            // ACK: PIO generates H; a zero payload produces the following L.
+            picoputerlinkout_program_putc(linkout_pio, linkout_sm, 0x000);
+            sleep_ms(10);
+            // Data: second start bit in bit 0, D0..D7 in bits 1..8, stop=0.
+            picoputerlinkout_program_putc(linkout_pio,
+                                           linkout_sm,
+                                           0x001 | ((data & 0xff) << 1));
+        }
+#endif
+    }
+
+    // And done for now
+    exit(0);
+#if 0
+  while (true)
+    {
+      // Blink
+      pio_sm_put_blocking(pio, sm, 1);
+      sleep_ms(100);
+      // Blonk
+      pio_sm_put_blocking(pio, sm, 0);
+      sleep_ms(100);
+    }
+#endif
+#endif
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offsetpclk = pio_add_program(pio, &picoputerclk_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint smclk = pio_claim_unused_sm(pio, true);
+
+    picoputerclk_program_init(pio, smclk, offsetpclk, TP3_LINK_CLOCK_PIN);
+    // The state machine is now running. Any value we push to its TX FIFO will
+
+    ////////////////////////////////////////////////////////////////////////////////
+
+    int count = 0;
+
+
+
+    // sd_test();
+
+    while (1)
+    {
+        char line[80];
+
+        sprintf(line, "Loop %d", count);
+        gpio_put(TP3_LED_PIN, 0);
+
+        sleep_ms(1000);
+        gpio_put(TP3_LED_PIN, 1);
+
+        puts(line);
+        sleep_ms(500);
+
+        oled_set_xy(&oled0, 0, 35);
+        oled_display_string(&oled0, line);
+
+        count++;
+    }
+}
---
+++ pico_transputer_config.h
@@ -0,0 +1,190 @@
+#ifndef PICO_TRANSPUTER_CONFIG_H
+#define PICO_TRANSPUTER_CONFIG_H
+
+#include <stdint.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/spi.h"
+
+// https://patorjk.com/software/taag/#p=display&f=Big&t=Memory&x=none&v=4&h=4&w=80&we=false
+
+
+
+/*
+*  _____ _____ _   _         _____             __ _       
+* |  __ \_   _| \ | |       / ____|           / _(_)      
+* | |__) || | |  \| |______| |     ___  _ __ | |_ _  __ _ 
+* |  ___/ | | | . ` |______| |    / _ \| '_ \|  _| |/ _` |
+* | |    _| |_| |\  |      | |___| (_) | | | | | | | (_| |
+* |_|   |_____|_| \_|       \_____\___/|_| |_|_| |_|\__, |
+*                                                    __/ |
+*                                                   |___/ 
+*
+* Raspberry Pi Pico 2 pin assignment for transputer03.
+ *
+ * Keep all board-level GPIO and peripheral choices in this file.  Driver
+ * headers should describe APIs, not select the pins used by this board.
+ *
+ * Signal                 GPIO    Pico 2 physical pin
+ * --------------------------------------------------
+ * Console UART TX        GP0     1
+ * Console UART RX        GP1     2
+ * OLED I2C0 SDA          GP4     6
+ * OLED I2C0 SCL          GP5     7
+ * SPI RAM MISO/RX        GP12    16
+ * SPI RAM CS             GP13    17
+ * SPI RAM SCK            GP14    19
+ * SPI RAM MOSI/TX        GP15    20
+ * Transputer LinkIn      GP16    21
+ * Transputer LinkOut     GP17    22
+ * Transputer LinkClock   GP18    24    5 MHz
+ * On-board LED           GP25    internal
+ */
+
+
+/* stdio UART is enabled in CMakeLists.txt and uses the Pico 2 defaults. */
+#define TP3_CONSOLE_UART_TX_PIN  PICO_DEFAULT_UART_TX_PIN
+#define TP3_CONSOLE_UART_RX_PIN  PICO_DEFAULT_UART_RX_PIN
+
+/* On-board status LED. */
+#define TP3_LED_PIN              PICO_DEFAULT_LED_PIN
+
+/* SSD1306 OLED display. GP4/GP5 are an I2C0 pin pair. */
+#define TP3_OLED_I2C             i2c0
+#define TP3_OLED_SDA_PIN         4u
+#define TP3_OLED_SCL_PIN         5u
+#define TP3_OLED_ADDRESS         0x3cu
+#define TP3_OLED_BAUDRATE        400000u
+
+/* Transputer serial link implemented by PIO0. */
+#define TP3_LINK_PIO             pio0
+#define TP3_LINK_IN_PIN          16u
+#define TP3_LINK_OUT_PIN         17u
+#define TP3_LINK_CLOCK_PIN       18u
+
+/* Optional external SPI RAM on SPI1. */
+#define TP3_SPI_RAM_SPI          spi1
+#define TP3_SPI_RAM_RX_PIN       12u
+#define TP3_SPI_RAM_CSN_PIN      13u
+#define TP3_SPI_RAM_SCK_PIN      14u
+#define TP3_SPI_RAM_TX_PIN       15u
+#define TP3_SPI_RAM_BAUDRATE     1000000u
+
+/*
+ * Catch accidental GPIO reuse when this file is edited.  The Pico SDK uses
+ * GCC/Clang, where __builtin_popcountll() is a constant expression here.
+ */
+#define TP3_GPIO_BIT(pin_)       (UINT64_C(1) << (pin_))
+#define TP3_USED_GPIO_MASK                                                   \
+    (TP3_GPIO_BIT(TP3_CONSOLE_UART_TX_PIN) |                                \
+     TP3_GPIO_BIT(TP3_CONSOLE_UART_RX_PIN) |                                \
+     TP3_GPIO_BIT(TP3_LED_PIN) |                                            \
+     TP3_GPIO_BIT(TP3_OLED_SDA_PIN) |                                       \
+     TP3_GPIO_BIT(TP3_OLED_SCL_PIN) |                                       \
+     TP3_GPIO_BIT(TP3_LINK_IN_PIN) |                                        \
+     TP3_GPIO_BIT(TP3_LINK_OUT_PIN) |                                       \
+     TP3_GPIO_BIT(TP3_LINK_CLOCK_PIN) |                                     \
+     TP3_GPIO_BIT(TP3_SPI_RAM_RX_PIN) |                                     \
+     TP3_GPIO_BIT(TP3_SPI_RAM_CSN_PIN) |                                    \
+     TP3_GPIO_BIT(TP3_SPI_RAM_SCK_PIN) |                                    \
+     TP3_GPIO_BIT(TP3_SPI_RAM_TX_PIN))
+
+_Static_assert(__builtin_popcountll(TP3_USED_GPIO_MASK) == 12,
+               "pico2_pin_config.h assigns one GPIO to multiple functions");
+
+
+
+/*
+*  _      _       _           _____                     _         _____             __ _       
+* | |    (_)     | |         / ____|                   | |       / ____|           / _(_)      
+* | |     _ _ __ | | _______| (___  _ __   ___  ___  __| |______| |     ___  _ __ | |_ _  __ _ 
+* | |    | | '_ \| |/ /______\___ \| '_ \ / _ \/ _ \/ _` |______| |    / _ \| '_ \|  _| |/ _` |
+* | |____| | | | |   <       ____) | |_) |  __/  __/ (_| |      | |___| (_) | | | | | | | (_| |
+* |______|_|_| |_|_|\_\     |_____/| .__/ \___|\___|\__,_|       \_____\___/|_| |_|_| |_|\__, |
+*                                  | |                                                    __/ |
+*                                  |_|                                                   |___/  
+*
+* RP2350 system clock.
+*
+* 160 MHz is required by the existing eight-PIO-cycles-per-bit
+* implementation for a 20 Mbit/s link.
+*
+* Pico 2 is officially specified for 150 MHz, so this is technically
+* an overclock.
+*/
+
+#define TP3_SYS_CLOCK_KHZ           160000u
+
+/*
+ * INMOS serial-link bit rate.
+ */
+#define TP3_LINK_BIT_RATE_HZ        20000000u
+
+/*
+ * The current LinkIn and LinkOut PIO programs use eight state-machine
+ * cycles for each serial-link bit.
+ */
+#define TP3_LINK_CYCLES_PER_BIT     8u
+
+#define TP3_LINK_PIO_CLOCK_HZ       \
+    (TP3_LINK_BIT_RATE_HZ * TP3_LINK_CYCLES_PER_BIT)
+
+/*
+ * The IMS C011 ClockIn remains 5 MHz for both the 10 Mbit/s and
+ * 20 Mbit/s link modes.
+ */
+#define TP3_C011_CLOCK_HZ           5000000u
+
+_Static_assert(
+    TP3_LINK_PIO_CLOCK_HZ <= TP3_SYS_CLOCK_KHZ * 1000u,
+    "System clock is too low for configured link rate"
+);
+
+
+/*
+*  __  __                                 
+* |  \/  |                                
+* | \  / | ___ _ __ ___   ___  _ __ _   _ 
+* | |\/| |/ _ \ '_ ` _ \ / _ \| '__| | | |
+* | |  | |  __/ | | | | | (_) | |  | |_| |
+* |_|  |_|\___|_| |_| |_|\___/|_|   \__, |
+*                                    __/ |
+*                                   |___/ 
+*
+* Select memory backend for the emulated transputer external memory.
+ *
+ * 0 = use Pico/RP2040/RP2350 internal RAM array in t4/p.c
+ * 1 = use external SPI RAM through helper/spi_ram.c
+ *
+ * Can also be overridden from CMake:
+ *   target_compile_definitions(transputer03 PRIVATE TRANS_USE_SPI_RAM=1)
+ */
+#ifndef TRANS_USE_SPI_RAM
+#define TRANS_USE_SPI_RAM 0
+#endif
+
+/* Transputer memory map base values used by the current emulator port. */
+#define TRANS_MEM_START       ((uint32_t)0x80000048u)
+#define TRANS_CORE_SIZE       ((uint32_t)(2u * 1024u))
+#define TRANS_EXT_MEM_START   ((uint32_t)0x80000800u)
+
+/*
+ * The current p.c memory access code uses address masking, so these sizes
+ * should be powers of two.
+ */
+#define TRANS_PICO_RAM_SIZE   ((uint32_t)(64u * 1024u))
+#define TRANS_SPI_RAM_SIZE    ((uint32_t)(2u * 1024u * 1024u))
+
+#if TRANS_USE_SPI_RAM
+#define TRANS_EXT_MEM_SIZE    TRANS_SPI_RAM_SIZE
+#define TRANS_MEM_BACKEND_NAME "external SPI RAM"
+#else
+#define TRANS_EXT_MEM_SIZE    TRANS_PICO_RAM_SIZE
+#define TRANS_MEM_BACKEND_NAME "Pico internal RAM"
+#endif
+
+#define TRANS_MEM_WORD_MASK   ((uint32_t)((TRANS_EXT_MEM_SIZE - 1u) & 0xFFFFFFFCu))
+#define TRANS_MEM_BYTE_MASK   ((uint32_t)(TRANS_EXT_MEM_SIZE - 1u))
+
+#endif /* PICO_TRANSPUTER_CONFIG_H */
